750ea26987e0e7cfaecfe5205caedd35e530ce76
whohaaa <109943477+whohaaa@users.noreply.github.com>
2022-07-26 12:02:17 +0800

    Add files via upload

---
+++ M&D Quiz.py
@@ -0,0 +1,40 @@
+print("Multiplication & Division in Decimals quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print("Okay! Let's play :)")
+
+answer = input("What is 26.75 x 20 ")
+if answer == "53.50":
+    print('correct answer! You got 3 marks!')
+else:
+    print("Incorrect! Do not give up! Try again!")
+
+answer = input("What is 8.484 divided by 4? ")
+if answer == "2.121":
+    print('correct answer! You got 3 marks!')
+else:
+    print("Incorrect! Do not give up! Try again!")
+
+answer = input("What is 4.620 x 300? ")
+if answer == "1386":
+    print('correct answer! You got 3 marks!')
+else:
+    print("Incorrect! Do not give up! Try again!")
+
+answer = input("5.005 divide by 5? ")
+if answer == "1.001":
+    print('correct answer! You got 3 marks!')
+else:
+    print("Incorrect! Do not give up! Try again!")
+
+answer = input("What is 21.34 x 4000? ")
+if answer == "85360":
+    print('correct answer! You got 3 marks!')
+else:
+    print("Incorrect! Do not give up! Try again!")
+
+print("The End!")
